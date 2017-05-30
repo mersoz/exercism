c4a1fb54d4809367628aed26a842c7b20b01da8f
Muge Ersoz <muge@ersoz.org>
2017-05-30 23:34:02 +0100

    Started JavaScript Exercisms

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,10 @@
+function Hamming() {
+
+};
+
+Hamming.prototype.compute = function (strand1, strand2) {
+  // strand1.forEach(() => {
+    console.log(strand1);
+    console.log(strand2);
+  // })
+};
